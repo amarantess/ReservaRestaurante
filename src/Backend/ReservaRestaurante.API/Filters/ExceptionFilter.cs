68162ca5fba83387c15bca7f40a15fd4f019af83
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:25:45 -0300

    feat: adding filter exception

---
+++ src/Backend/ReservaRestaurante.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ReservaRestaurante.Communication.Responses;
+using ReservaRestaurante.Exceptions;
+using ReservaRestaurante.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace ReservaRestaurante.API.Filters
+{
+	public class ExceptionFilter : IExceptionFilter
+	{
+		public void OnException(ExceptionContext context)
+		{
+			if(context.Exception is ReservaRestauranteException)
+			{
+				HandleProjectException(context);
+			}
+			else
+			{
+				ThrowUnknowException(context);
+			}
+		}
+
+		private void HandleProjectException(ExceptionContext context)
+		{
+			if(context.Exception is ErrorOnValidationException)
+			{
+				var exception = context.Exception as ErrorOnValidationException;
+
+				context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+				context.Result = new BadRequestObjectResult(new ResponseError(exception.ErrorMessages));
+			}
+		}
+
+		private static void ThrowUnknowException(ExceptionContext context)
+		{
+			context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+			context.Result = new ObjectResult(new ResponseError(ResourceMessagesException.UNKNOW_ERROR));
+		}
+	}
+}

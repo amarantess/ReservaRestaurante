8ccdb81a76e1a8695b0bc2a820c2156ad3d3aa9d
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:07:28 -0300

    feat: adding response error

---
+++ src/Shared/ReservaRestaurante.Communication/Responses/ResponseError.cs
@@ -0,0 +1,15 @@
+﻿namespace ReservaRestaurante.Communication.Responses
+{
+	public class ResponseError
+	{
+		public IList<string> Errors { get; set; }
+
+		public ResponseError(IList<string> errors) => Errors = errors;
+
+		public ResponseError(string error)
+		{
+			Errors = new List<string>();
+			Errors.Add(error);
+		}
+	}
+}

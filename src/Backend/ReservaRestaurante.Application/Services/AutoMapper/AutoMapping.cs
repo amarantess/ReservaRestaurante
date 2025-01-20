1c5aece05bfd0d8663fd38f6a50611af81d68a49
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:21:02 -0300

    feat: adding AutoMapper profile

---
+++ src/Backend/ReservaRestaurante.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ReservaRestaurante.Communication.Requests;
+using ReservaRestaurante.Domain.Entities;
+
+namespace ReservaRestaurante.Application.Services.AutoMapper
+{
+	public class AutoMapping : Profile
+	{
+		public AutoMapping()
+		{
+			RequestToDomain();
+		}
+
+		private void RequestToDomain()
+		{
+			CreateMap<RequestRegisterUser, User>()
+				.ForMember(dest => dest.Password, options => options.Ignore());
+		}
+	}
+}

ff1be90a39c6f0c625c3596e74a0a87536929326
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:22:15 -0300

    feat: adding password encripter

---
+++ src/Backend/ReservaRestaurante.Application/Services/Criptography/PasswordEncripter.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ReservaRestaurante.Application.Services.Criptography
+{
+	public class PasswordEncripter
+	{
+		public string Encrypt(string password)
+		{
+			var bytes = Encoding.UTF8.GetBytes(password);
+			var hashBytes = SHA512.HashData(bytes);
+
+			return StringBytes(bytes);
+		}
+
+		private static string StringBytes(byte[] bytes)
+		{
+			var sb = new StringBuilder();
+			foreach (byte b in bytes)
+			{
+				var hex = b.ToString("x2");
+				sb.Append(hex);
+			}
+
+			return sb.ToString();
+		}
+	}
+}

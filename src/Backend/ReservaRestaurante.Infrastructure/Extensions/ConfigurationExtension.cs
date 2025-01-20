dd9eb462b57e4e81bf8ce7cb6e2a1b5eb51aa513
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:29:20 -0300

    feat: creating schema or database when api runs

---
+++ src/Backend/ReservaRestaurante.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ReservaRestaurante.Infrastructure.Extensions
+{
+	public static class ConfigurationExtension
+	{
+		public static string ConnectionString(this IConfiguration configuration)
+		{
+			return configuration.GetConnectionString("DefaultConnection")!;
+		}
+	}
+}

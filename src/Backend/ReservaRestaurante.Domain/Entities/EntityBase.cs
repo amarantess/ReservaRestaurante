113bc88565138cff4e5d6a634c2ac5d73d4a65fa
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-20 18:12:12 -0300

    feat: adding user entity

---
+++ src/Backend/ReservaRestaurante.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservaRestaurante.Domain.Entities
+{
+	public class EntityBase
+	{
+		public long Id { get; set; }
+		public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+	}
+}
---
+++ src/Backend/ReservaRestaurante.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservaRestaurante.Domain.Entities
+{
+	public class User : EntityBase
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public string Role { get; set; } = "Client"; // Client or Administrator
+	}
+}

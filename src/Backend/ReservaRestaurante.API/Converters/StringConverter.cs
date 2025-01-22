1e67444665dc8f10b2e77e08d840f9c49b138edc
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-22 19:14:28 -0300

    feat: adding string converter for json

---
+++ src/Backend/ReservaRestaurante.API/Converters/StringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace ReservaRestaurante.API.Converters
+{
+	public partial class StringConverter : JsonConverter<string>
+	{
+		public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			var value = reader.GetString()?.Trim();
+
+			if (value == null) 
+				return null;
+
+			return RemoveExtraWhiteSpaces().Replace(value, " "); //  Recebe a string e se encontrar um amontoado de espaços em branco será trocado por apenas um espaço.
+		}
+
+		public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) => writer.WriteStringValue(value);
+
+		[GeneratedRegex(@"\s+")]
+		private static partial Regex RemoveExtraWhiteSpaces();
+	}
+}
--- src/Backend/ReservaRestaurante.API/Program.cs
+++ src/Backend/ReservaRestaurante.API/Program.cs
@@ -1,3 +1,4 @@
+using ReservaRestaurante.API.Converters;
 using ReservaRestaurante.API.Filters;
 using ReservaRestaurante.Application;
 using ReservaRestaurante.Infrastructure;
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

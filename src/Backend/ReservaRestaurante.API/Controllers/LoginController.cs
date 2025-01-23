591d8d84a4fca7682e3b1dd6b074a584bebc3beb
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-23 10:20:49 -0300

    feat: adding endpoint to do login

---
+++ src/Backend/ReservaRestaurante.API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReservaRestaurante.Application.UseCases.Login.DoLogin;
+using ReservaRestaurante.Communication.Requests;
+using ReservaRestaurante.Communication.Responses;
+
+namespace ReservaRestaurante.API.Controllers
+{
+	[Route("login")]
+	[ApiController]
+	public class LoginController : ControllerBase
+	{
+		[HttpPost]
+		[ProducesResponseType(typeof(ResponseRegisteredUser), StatusCodes.Status200OK)]
+		[ProducesResponseType(typeof(ResponseError), StatusCodes.Status401Unauthorized)]
+		public async Task<ActionResult> Login([FromServices] IDoLoginUseCase useCase, [FromBody] RequestLogin request)
+		{
+			var response = await useCase.Execute(request);
+
+			return Ok(response);
+		}
+	}
+}

08308ac07197fa5c866098210da3e515ce482cbe
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-01-26 18:17:04 -0300

    feat: make a static method

--- src/Backend/ReservaRestaurante.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
+++ src/Backend/ReservaRestaurante.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -5,7 +5,7 @@ namespace ReservaRestaurante.Infrastructure.Security.Tokens.Access
 {
 	public abstract class JwtTokenHandler
 	{
-		protected SymmetricSecurityKey SecurityKey(string signingKey) // Transformando a assinatura em uma securityKey
+		protected static SymmetricSecurityKey SecurityKey(string signingKey) // Transformando a assinatura em uma securityKey
 		{
 			var bytes = Encoding.UTF8.GetBytes(signingKey);
 

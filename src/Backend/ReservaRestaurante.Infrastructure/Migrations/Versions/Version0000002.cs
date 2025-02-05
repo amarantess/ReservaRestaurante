4352bd04706a92dda14a9d9ea32d758fe234509f
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-05 16:48:30 -0300

    feat: adding migration to create table of tables

---
+++ src/Backend/ReservaRestaurante.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace ReservaRestaurante.Infrastructure.Migrations.Versions
+{
+	[Migration(2, "Create table to save table's information")]
+	public class Version0000002 : VersionBase
+	{
+		public override void Up()
+		{
+			CreateTable("Table")
+				.WithColumn("Number").AsInt32().NotNullable()
+				.WithColumn("Capacity").AsInt32().NotNullable()
+				.WithColumn("Status").AsString(50).NotNullable();
+		}
+	}
+}

623d2e93a21f1637f4038da51bcde86b45466236
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-06 18:15:46 -0300

    feat: adding table entity

---
+++ src/Backend/ReservaRestaurante.Domain/Entities/Table.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaRestaurante.Domain.Entities
+{
+	public class Table : EntityBase
+	{
+		public int Number {  get; set; }
+		public int Capacity { get; set; }
+		public string Status { get; set; } = "Available"; // Available, Reserved, Inactive
+	}
+}

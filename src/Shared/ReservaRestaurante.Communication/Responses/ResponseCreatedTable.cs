8a82250e7a1dd22c8de57b9af5d9bfadd75bf265
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-06 18:13:27 -0300

    feat: adding response to create table

---
+++ src/Shared/ReservaRestaurante.Communication/Responses/ResponseCreatedTable.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservaRestaurante.Communication.Responses
+{
+	public class ResponseCreatedTable
+	{
+		public long Id { get; set; }
+		public DateTime CreatedOn { get; set; }
+		public int Number { get; set; }
+		public int Capacity { get; set; }
+		public string Status { get; set; } = string.Empty;
+	}
+}

c9e4e3c57fd97e9efe1210bddc25880a4e491db9
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-07 16:01:35 -0300

    tests: unit tests to list tables for admins

---
+++ tests/UseCases.Test/Table/List_Admin/ListTableAdminUseCaseTest.cs
@@ -0,0 +1,33 @@
+﻿using CommomTestUtilities.Entities;
+using CommomTestUtilities.Mapper;
+using CommomTestUtilities.Repositories;
+using FluentAssertions;
+using ReservaRestaurante.Application.UseCases.Table.List_Admin;
+
+namespace UseCases.Test.Table.List_Admin
+{
+	public class ListTableAdminUseCaseTest
+	{
+		[Fact]
+		public async Task Success()
+		{
+			//Arrange
+			var table = TableBuilder.Build();
+			var useCase = CreateUseCase(table);
+
+			//Act
+			var result = await useCase.Execute();
+
+			//Arrange
+			result.Should().NotBeNull();
+		}
+
+		private static ListTableAdminUseCase CreateUseCase(ReservaRestaurante.Domain.Entities.Table table)
+		{
+			var repository = new TableReadOnlyRepositoryBuilder();
+			var mapper = MapperBuilder.Build();
+
+			return new ListTableAdminUseCase(repository.Build(), mapper);
+		}
+	}
+}

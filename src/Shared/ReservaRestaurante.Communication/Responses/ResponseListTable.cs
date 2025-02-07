477df5c53236b6791e57721a75da6246ee7443f0
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-07 14:56:27 -0300

    feat: adding response to list tables

---
+++ src/Shared/ReservaRestaurante.Communication/Responses/ResponseListTable.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaRestaurante.Communication.Responses
+{
+	public class ResponseListTable
+	{
+		public int Number { get; set; }
+		public int Capacity { get; set; }
+		public string Status { get; set; } = string.Empty;
+	}
+}

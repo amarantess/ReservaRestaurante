cb66ce7afebe61ffab257c9fe890840bf70e6e49
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-08 19:41:55 -0300

    feat: adding request to update table

---
+++ src/Shared/ReservaRestaurante.Communication/Requests/RequestUpdateTable.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservaRestaurante.Communication.Requests
+{
+	public class RequestUpdateTable
+	{
+		public int Capacity { get; set; }
+		public string Status { get; set; } = string.Empty;
+	}
+}

23a0793742d09a0bc58c91d52947a2c1d389853f
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-09 18:32:55 -0300

    tests: unit tests to delete table

---
+++ tests/UseCases.Test/Table/Delete/DeleteTableUseCaseTest.cs
@@ -0,0 +1,54 @@
+﻿using CommomTestUtilities.Repositories;
+using FluentAssertions;
+using ReservaRestaurante.Application.UseCases.Table.Delete;
+using ReservaRestaurante.Exceptions;
+using ReservaRestaurante.Exceptions.ExceptionsBase;
+
+namespace UseCases.Test.Table.Delete
+{
+	public class DeleteTableUseCaseTest
+	{
+		[Fact]
+		public async Task Success()
+		{
+			//Arrange
+			long id = 1;
+			var useCase = CreateUseCase(id);
+
+			//Act
+			var act = async () => await useCase.Execute(id);
+
+			//Assert
+			await act.Should().NotThrowAsync();
+		}
+
+		[Fact]
+		public async Task Table_NotFound()
+		{
+			//Arrange
+			long id = 0;
+			var useCase = CreateUseCase(id);
+
+			//Act
+			var act = async () => await useCase.Execute(id);
+
+			//Assert
+			(await act.Should().ThrowAsync<NotFoundException>())
+			.Where(e => e.Message.Equals(ResourceMessagesException.TABLE_NOT_FOUND));
+		}
+
+		private static DeleteTableUseCase CreateUseCase(long id = 0)
+		{
+			var writeRepositoyBuilder = TableWriteOnlyRepositoryBuilder.Build();
+			var readRepositoryBuilder = new TableReadOnlyRepositoryBuilder();
+			var unitOfWork = UnitOfWorkBuilder.Build();
+
+			if (id != 0)
+			{
+				readRepositoryBuilder.ExistTableWithId(id);
+			}
+
+			return new DeleteTableUseCase(writeRepositoyBuilder, readRepositoryBuilder.Build(), unitOfWork);
+		}
+	}
+}
---
+++ tests/WebApi.Test/Table/Delete/DeleteTableTest.cs
@@ -0,0 +1,53 @@
+﻿using CommomTestUtilities.Tokens;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace WebApi.Test.Table.Delete
+{
+	public class DeleteTableTest : IClassFixture<CustomWebApplicationFactory>
+	{
+		private readonly HttpClient _httpClient;
+		private readonly Guid _userIdentifier;
+
+		public DeleteTableTest(CustomWebApplicationFactory factory)
+		{
+			_httpClient = factory.CreateClient();
+			_userIdentifier = factory.GetUserIdentifier();
+		}
+
+		[Fact]
+		public async Task Success()
+		{
+			long id = 1;
+			var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+			AuthorizeRequest(token);
+
+			var response = await _httpClient.DeleteAsync($"tables/{id}");
+
+			response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+		}
+
+		[Fact]
+		public async Task Table_NotFound()
+		{
+			long id = 0;
+			var token = JwtTokenGeneratorBuilder.Build().Generate(_userIdentifier);
+
+			AuthorizeRequest(token);
+
+			var response = await _httpClient.DeleteAsync($"tables/{id}");
+
+			response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+		}
+
+		private void AuthorizeRequest(string token)
+		{
+			if (string.IsNullOrWhiteSpace(token))
+				return;
+
+			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+		}
+	}
+}

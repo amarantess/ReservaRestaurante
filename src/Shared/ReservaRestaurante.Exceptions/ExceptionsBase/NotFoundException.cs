7b4ef684d4b24d67880908af3c3d5bfe22794d98
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-09 18:27:19 -0300

    feat: adding not found exception

---
+++ src/Shared/ReservaRestaurante.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaRestaurante.Exceptions.ExceptionsBase
+{
+	public class NotFoundException : ReservaRestauranteException
+	{
+		public NotFoundException(string message) : base(message)
+		{
+		}
+	}
+}

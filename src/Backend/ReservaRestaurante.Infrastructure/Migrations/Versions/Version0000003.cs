bef3d4ff4e2ad2c094c97be177c7cc6bd1da7d67
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-13 00:03:53 -0300

    feat: adding migration to create table of reservation

---
+++ src/Backend/ReservaRestaurante.Infrastructure/Migrations/Versions/Version0000003.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace ReservaRestaurante.Infrastructure.Migrations.Versions
+{
+	[Migration(4, "Create table to save reservation information")]
+	public class Version0000003 : VersionBase
+	{
+		public override void Up()
+		{
+			CreateTable("Reservations")
+				.WithColumn("UserId").AsInt64().NotNullable().ForeignKey("FK_Reservation_User_Id", "Users", "Id")
+				.WithColumn("TableId").AsInt64().NotNullable().ForeignKey("FK_Reservation_Table_Id", "Table", "Id")
+				.WithColumn("ReservationDate").AsDateTime().NotNullable()
+				.WithColumn("Status").AsString(50).NotNullable();
+		}
+	}
+}

4df849de450eefa7f7b9ea43081c224209a1dfd8
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-12 23:59:10 -0300

    feat: adding custom exceptions to use in the create reservation flow

---
+++ src/Shared/ReservaRestaurante.Exceptions/ExceptionsBase/CapacityInvalidException.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaRestaurante.Exceptions.ExceptionsBase
+{
+	public class CapacityInvalidException : ReservaRestauranteException
+	{
+		public CapacityInvalidException() : base(ResourceMessagesException.TABLE_NOT_SUPPORT)
+		{
+		}
+	}
+}
---
+++ src/Shared/ReservaRestaurante.Exceptions/ExceptionsBase/TableIsNotAvailableException.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaRestaurante.Exceptions.ExceptionsBase
+{
+	public class TableIsNotAvailableException : ReservaRestauranteException
+	{
+		public TableIsNotAvailableException() : base(ResourceMessagesException.TABLE_NOT_AVAILABLE)
+		{
+		}
+	}
+}

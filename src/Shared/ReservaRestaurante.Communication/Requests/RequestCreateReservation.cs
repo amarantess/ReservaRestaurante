40aba486d3835eb0a893e7e5283f92da90eb0253
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-12 23:51:44 -0300

    feat: adding request to create reservation

---
+++ src/Shared/ReservaRestaurante.Communication/Requests/RequestCreateReservation.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ReservaRestaurante.Communication.Requests
+{
+	public class RequestCreateReservation
+	{
+		public int TableNumber { get; set; }
+		public string ReservationDateTime { get; set; } = string.Empty;
+		public int PeopleNumber { get; set; }
+	}
+}

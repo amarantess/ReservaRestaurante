5198f4c20824209e10fd1c4b5e65ea5482884acf
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-13 00:01:51 -0300

    feat: adding reservation entity

---
+++ src/Backend/ReservaRestaurante.Domain/Entities/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservaRestaurante.Domain.Entities
+{
+	public class Reservation : EntityBase
+	{
+		public long UserId { get; set; }
+		public long TableId { get; set; }
+		public DateTime ReservationDate { get; set; }
+		public string Status { get; set; } = "Active"; // Active or Canceled
+	}
+}

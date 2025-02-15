34c4a05f8e7bfa9368240f320fab692a584036fe
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-02-15 19:47:42 -0300

    feat: adding response to list user's reservations

---
+++ src/Shared/ReservaRestaurante.Communication/Responses/ResponseListReservation.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservaRestaurante.Communication.Responses
+{
+    public class ResponseListReservation
+    {
+        public string ReservationDate { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+	}
+}

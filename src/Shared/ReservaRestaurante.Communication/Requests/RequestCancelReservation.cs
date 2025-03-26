32c385ba01b98ad3ff5cf098d7e6f23e96bb99f1
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-03-26 18:09:16 -0300

    feat: adding request to cancel reservation

---
+++ src/Shared/ReservaRestaurante.Communication/Requests/RequestCancelReservation.cs
@@ -0,0 +1,8 @@
+﻿namespace ReservaRestaurante.Communication.Requests
+{
+    public class RequestCancelReservation
+    {
+		public int TableNumber { get; set; }
+		public string ReservationDateTime { get; set; } = string.Empty;
+	}
+}

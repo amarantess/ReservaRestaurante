04743eedb880ff4f69bb920bee415fa164fdbac9
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2025-06-05 22:05:01 -0300

    fix: fixing the token controller

--- src/Backend/ReservaRestaurante.API/Controllers/TokenController.cs
+++ src/Backend/ReservaRestaurante.API/Controllers/TokenController.cs
@@ -9,18 +9,12 @@ namespace ReservaRestaurante.API.Controllers
     [ApiController]
     public class TokenController : ControllerBase
     {
-        private readonly IUseRefreshTokenUseCase _service;
-
-		public TokenController(IUseRefreshTokenUseCase service)
-		{
-			_service = service;
-		}
 
 		[HttpPost("refresh-token")]
         [ProducesResponseType(typeof(ResponseTokenJson), StatusCodes.Status200OK)]
         public async Task<IActionResult> RefreshToken([FromServices] IUseRefreshTokenUseCase useCase, [FromBody] RequestNewTokenJson request)
         {
-            var response = await _service.Execute(request);
+            var response = await useCase.Execute(request);
 
             return Ok(response);
         }
